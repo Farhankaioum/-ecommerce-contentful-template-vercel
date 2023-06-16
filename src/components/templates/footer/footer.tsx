87578a7240f251df4502eaf56ac25e7f7a918c8e
keiuom <keiuom.miah@wundermanthompson.com>
2023-06-16 19:07:13 +0600

    footer changes

--- src/components/templates/footer/footer.tsx
+++ src/components/templates/footer/footer.tsx
@@ -12,11 +12,24 @@ export const Footer = () => {
       py={{ base: 10, lg: 16 }}
       mt="auto"
       borderTop="1px"
-      borderColor={theme.f36.gray200}>
+      borderColor={theme.f36.gray200}
+    >
       <Container>
         <Text mb={8}>{t('common.aboutUs')}</Text>
         <Text>{t('common.description1')}</Text>
-        <Text mb={8}>{t('common.description2')}</Text>
+        <Text>{t('common.description2')}</Text>
+        <Text>
+          Developed as part of <span style={{ color: 'red', fontSize: '16px' }}>MACHATHON</span> by
+          <Link
+            marginLeft={1}
+            href="https://www.wundermanthompson.com/bangladesh"
+            isExternal
+            color={theme.f36.blue500}
+          >
+            Wunderman Thompson Dhaka
+          </Link>
+        </Text>
+        <Text mb={8}>{t('common.teamName')}</Text>
         <Text variant="small">
           {t('common.poweredBy')}{' '}
           <Link href="https://contentful.com" isExternal color={theme.f36.blue500}>
